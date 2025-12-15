1ddcd565a924fc2129bec02a96a1cc889c630e77
JulianoAleixo <juliano.moreira.aleixo@gmail.com>
2025-12-15 11:54:48 -0300

    :sparkles: Feat - Adding Seller class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Department department = new Department(1, "Books");
+        System.out.println(department);
+
+        Seller seller = new Seller(1, "Juliano", "juliano@email.com", new Date(), 2500.0, department);
+        System.out.println(seller);
     }
 }
